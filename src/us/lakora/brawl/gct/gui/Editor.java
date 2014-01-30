8499fb766015b9682635d8f8bb4a03c8efb601c3
libertyernie <libertyernie0@gmail.com>
2014-01-30 13:07:12 -0600

    change license from gpl2 to gpl3

--- src/us/lakora/brawl/gct/gui/Editor.java
+++ src/us/lakora/brawl/gct/gui/Editor.java
@@ -61,7 +61,7 @@ public class Editor extends JFrame {
 			"\n" +
 			"This program is free software: you can redistribute it and/or modify\n" +
 			"it under the terms of the GNU General Public License as published by\n" +
-			"the Free Software Foundation, either version 2 of the License, or\n" +
+			"the Free Software Foundation, either version 3 of the License, or\n" +
 			"(at your option) any later version.\n\n" +
 			"This program is distributed in the hope that it will be useful,\n" +
 			"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
